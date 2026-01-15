3d0d950edd0ee2f2111aeeb6e0b1f4a5805b0cea
mehran <mehranmohamadi1311@gmail.com>
2026-01-15 16:33:11 +0330

    style: change input-range__wrapper box-shadow color

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 // Selecting Elements;
 const inputs = document.querySelectorAll(".input-range__wrapper input");
 const img = document.querySelector("#cr7");
-const values = document.querySelectorAll(".indicatore");
+const indicatores = document.querySelectorAll(".indicatore");
 
 const inputFile = document.querySelector("#input-file");
 const bgColorName = document.querySelector("#bgColorName");
 
 // Set dynamiclly styles;
-const updateStyle = function () {
+const updateStyle = function (e) {
   const ID = this.id;
   const suffix = ID === "base" ? "" : ID === "border" ? "%" : "px";
 
@@ -20,12 +20,12 @@ inputs.forEach((input) => input.addEventListener("mousemove", updateStyle));
 // Show the value of range;
 inputs.forEach((input, i) => {
   input.addEventListener("input", () => {
-    if (input.type == "range") {
-      const inputValue = input.value < 10 ? "0" + input.value : input.value;
-      values[i].textContent = inputValue;
-      values[i].style.left = `${(input.value / input.max) * 100}%`;
-      values[i].classList.add("show");
-    } else if (input.type == "color") {
+    if (input.type === "range") {
+      const inputValue = input.value < 10 ? `0${input.value}` : input.value;
+      indicatores[i].textContent = inputValue;
+      indicatores[i].style.left = `${(input.value / input.max) * 100}%`;
+      indicatores[i].classList.add("show");
+    } else if (input.type === "color") {
       bgColorName.textContent = input.value;
     }
   });
@@ -34,8 +34,8 @@ inputs.forEach((input, i) => {
 // onBlur;
 inputs.forEach((input, i) => {
   input.addEventListener("blur", () => {
-    if (input.type == "range") {
-      values[i].classList.remove("show");
+    if (input.type === "range") {
+      indicatores[i].classList.remove("show");
     }
   });
 });
